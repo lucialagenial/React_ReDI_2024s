b672081d1d33bad1a70b28df956a43a595a5eadf
Lucía Morales Lizárraga <69418959+lucialagenial@users.noreply.github.com>
2024-04-05 10:30:44 +0200

    Add files via upload

---
+++ session_5/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Counter from '../components/Counter'
+
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <Counter/>
+    </div>
+  )
+}
+
+export default App
